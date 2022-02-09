7da599d74a0d5c5e4065354e5ee581dba970e489
CM.Bae <gilddongii@gmail.com>
2022-02-09 14:33:34 +0900

    first commit

---
+++ gocoder/routes/form.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/',function(req,res,next){
+    res.render('form',{
+        name:'Bae Cheong Myeong',
+        blog:'gocoder.tistory.com',
+        homepage:'gocoder.net'
+    });
+});
+router.post('/',function(req,res,next){
+    res.json(req.body);
+});
+
+module.exports = router;
